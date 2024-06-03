703520989f3b34f746d519ceb72f5afae2589733
wolfvain305 <dwolf0305@gmail.com>
2024-06-02 22:31:49 -0400

    2

--- js/app.js
+++ js/app.js
@@ -1,26 +1,9 @@
 /*-------------------------------- Constants --------------------------------*/
 
-// Pseudocode
-
-
-
-//2) Store cached element references.
-
-//3) Upon loading, the game state should be initialized, and a function should 
-//   be called to render this game state.
-
-//4) The state of the game should be rendered to the user.
-
-//5) Define the required constants.
-
-//6) Handle a player clicking a square with a `handleClick` function.
-
-//7) Create Reset functionality.
-
-
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.getElementById('.message');
+const resetBtnEL = document.getElementById('reset')
 /*---------------------------- Variables (state) ----------------------------*/
-
-//1) Define the required variables used to track the state of the game.
 
 let board = ['','','',
              '','','',
@@ -31,9 +14,7 @@ let tie = false;
 
 /*------------------------ Cached Element References ------------------------*/
 
-const squareEls = document.querySelectorAll('.sqr');
-const messageEl = document.getElementById('.message');
-const resetBtnEL = document.getElementById('reset')
+
 
 /*-------------------------------- Functions --------------------------------*/
 
